6289aefb920f0b6fb1333d732d493dabaa7c694f
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-04-25 17:22:48 -0600

    Refactor Categories and SingleCategory components

--- Ecomerce/src/components/Categories.tsx
+++ Ecomerce/src/components/Categories.tsx
@@ -1,3 +1,17 @@
-export default function Categories({}) {
-  return <></>;
+import { useGetCategories } from "../hooks/useGetCategories";
+import { categoryType } from "../types/Category";
+import SingleCategory from "./SingleCategory";
+
+export default function Categories() {
+  const { categories } = useGetCategories()
+  return <>
+    <section>
+      {
+        categories.map((categorysResult: categoryType) => (
+          <SingleCategory key={categorysResult.id} category={categorysResult} >
+          </SingleCategory>
+        ))
+      }
+    </section>
+  </>;
 }
--- Ecomerce/src/components/SingleCategory.tsx
+++ Ecomerce/src/components/SingleCategory.tsx
@@ -1,3 +1,8 @@
-export default function SingleCategory({}) {
-  return <></>;
+import { categoryType } from "../types/Category";
+export default function SingleCategory({ category }: { category: categoryType }) {
+  return <>
+    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl flex justify-center">
+      {category.name}
+    </h1>
+  </>
 }
