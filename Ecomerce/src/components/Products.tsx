52168cc7f1d5ad028e09546e28f6c6967d4ee3d4
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-05-01 15:10:08 -0600

    Refactor Products component to use useGetProductByCategory hook and update dependencies

--- Ecomerce/src/components/Products.tsx
+++ Ecomerce/src/components/Products.tsx
@@ -1,21 +1,16 @@
 import { productType } from "../types/Product";
 import SingleProduct from "./SingleProduct";
 import { useGetProduts } from "../hooks/useGetProducts";
-import { useGetProductByCategory } from "../hooks/useGetProductByCategory";
 
 export default function Products() {
   const { products } = useGetProduts();
-  const { productByCategory, showByCategory } = useGetProductByCategory();
-
-  const productsList = showByCategory ? productByCategory : products
-
   return (
     <>
       <section className="grid grid-cols-4 gap-y-5">
-        {productsList?.map((productResult) => (
+        {products?.map((productResult:productType) => (
           <SingleProduct
             key={productResult.id}
-            product={productResult as productType}
+            product={productResult}
           ></SingleProduct>
         ))}
       </section>
