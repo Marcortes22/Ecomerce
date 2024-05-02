865099d1167d3f6f6f6699cf4a2ba4acd79c1a73
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2024-05-01 18:19:10 -0600

    Refactor Home component to remove unused code and update dependencies

--- Ecomerce/src/pages/Home.tsx
+++ Ecomerce/src/pages/Home.tsx
@@ -1,12 +1,4 @@
-import Categories from "../components/Categories";
-import Products from "../components/Products";
-import ProductsProvider from "../context/ProductsProvider";
-
 export default function Home() {
   return <>
-    <ProductsProvider>
-      <Categories />
-      <Products />
-    </ProductsProvider>
   </>;
 }
