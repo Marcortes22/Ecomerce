57f95c2dfa383378c094d936426486e4dfe8cb72
Marco Cortes <104529129+marcortes20@users.noreply.github.com>
2024-05-08 13:29:24 -0600

    products responsive view

--- Ecomerce/src/components/Products.tsx
+++ Ecomerce/src/components/Products.tsx
@@ -1,20 +1,19 @@
-import { productType } from '../types/Product'
-import SingleProduct from './SingleProduct'
-import { useGetProduts } from '../hooks/Products/useGetProducts'
-import { Link } from 'react-router-dom'
+import { productType } from "../types/Product";
+import SingleProduct from "./SingleProduct";
+import { useGetProduts } from "../hooks/Products/useGetProducts";
+import { Link } from "react-router-dom";
 
 export default function Products() {
-
-  const { filteredProducts } = useGetProduts()
+  const { filteredProducts } = useGetProduts();
 
   return (
     <>
-      <div className=' w-full'>
-        <button className='mx-4 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-green-400 mt-2'>
-          <Link to='/new'>Add</Link>
+      <div className=" w-full flex items-center justify-center">
+        <button className=" mt-[20px] mx-4 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-green-400  w-[100px]">
+          <Link to="/new">Add</Link>
         </button>
       </div>
-      <section className='grid grid-cols-4 gap-y-5 mt-5'>
+      <section className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-5 mt-5 place-items-center">
         {filteredProducts?.map((productResult: productType) => (
           <SingleProduct
             key={productResult.id}
@@ -23,5 +22,5 @@ export default function Products() {
         ))}
       </section>
     </>
-  )
+  );
 }
--- Ecomerce/src/components/SingleCategory.tsx
+++ Ecomerce/src/components/SingleCategory.tsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { categoryType } from "../types/Category";
 import ProductsContext from "../context/ProductsContext";
-import { set } from "react-hook-form";
 
 export default function SingleCategory({
   category,
@@ -20,7 +19,7 @@ export default function SingleCategory({
     <>
       <li
         onClick={() => {
-          handleClick;
+          handleClick();
           setIsOpen(false);
         }}
         className="relative py-2 px-3  text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 cursor-pointer"
