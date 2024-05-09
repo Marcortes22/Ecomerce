39809f9e252b44e8354647608a196be066a678d5
Marco Cortes <104529129+marcortes20@users.noreply.github.com>
2024-05-09 15:20:28 -0600

    feat: solve pagination bugs

--- Ecomerce/src/context/ProductsProvider.tsx
+++ Ecomerce/src/context/ProductsProvider.tsx
@@ -1,14 +1,24 @@
-import { ReactNode, useState } from 'react'
-import ProductsContext from './ProductsContext'
+import { ReactNode, useState } from "react";
+import ProductsContext from "./ProductsContext";
 
 const ProductsProvider = ({ children }: { children: ReactNode }) => {
-    const [categoryId, setCategoryId] = useState(0)
-    const [searchText, setSearchText] = useState('')
-    return (
-        <ProductsContext.Provider value={{ categoryId, setCategoryId, searchText, setSearchText }}>
-            {children}
-        </ProductsContext.Provider>
-    )
-}
+  const [categoryId, setCategoryId] = useState(0);
+  const [searchText, setSearchText] = useState("");
+  const [page, setPage] = useState(1);
+  return (
+    <ProductsContext.Provider
+      value={{
+        categoryId,
+        setCategoryId,
+        searchText,
+        setSearchText,
+        page,
+        setPage,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
 
-export default ProductsProvider
+export default ProductsProvider;
